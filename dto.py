224265870322cff3b05a6c43ed923e9fbe916257
wfsiew-nova <siewwingfei@hotmail.com>
2026-09-13 01:34:26 +0800

    u

--- dto.py
+++ dto.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PositiveInt
 
 
 class LoginDto(BaseModel):
@@ -8,6 +8,10 @@ class LoginDto(BaseModel):
     
 class RefreshTokenDto(BaseModel):
     refresh_token: str = Field(..., min_length=1)
+    
+class ChangePasswordDto(BaseModel):
+    password: str = Field(..., min_length=1, max_length=150)
+    confirm_password: str = Field(..., min_length=1, max_length=150)
 
 @dataclass
 class KeywordDto:
@@ -17,3 +21,17 @@ class CommonSetupDto(BaseModel):
     code: str = Field(..., min_length=1, max_length=30, str_strip=True)
     desc: str = Field(..., min_length=1, max_length=300, str_strip=True)
     ref: str = Field(..., min_length=1, max_length=200, str_strip=True)
+    
+class UserDto(BaseModel):
+    username: str = Field(..., min_length=1, max_length=150)
+    password: str = Field(..., min_length=1, max_length=150)
+    first_name: str = Field(..., min_length=1, max_length=150)
+    last_name: str = Field(..., max_length=150)
+    role_id: PositiveInt
+    
+class ReportQueryDto(BaseModel):
+    _page: PositiveInt
+    _limit: PositiveInt
+    vt: PositiveInt
+    datefrom: str = Field(..., min_length=1)
+    dateto: str = Field(..., min_length=1)
